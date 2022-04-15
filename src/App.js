e458d40c809e4c1b1a10fd4ca164ac7b8b7f812f
busrauygur <bnuygur@gmail.com>
2022-04-15 11:44:24 -0700

    Complete Projects

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
+import Projects from './components/Projects';
 import Contact from './components/Contact';
 
 function App() {
 	return (
 		<div>
 			<div className="sections">
-				<Navbar /> <Header /> <Skills /> <Experience /> <Contact />
+				<Navbar /> <Header /> <Skills /> <Experience /> <Projects /> <Contact />
 			</div>
 		</div>
 	);
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,35 +10,35 @@ const Skills = () => {
 				<div className="row">
 					<h1 className="skills-header col-lg-12">Skills & Tools</h1>
 					<div className="skills-box col-lg-3 col-sm-6">
-						<FontAwesomeIcon className="icon" icon={faHtml5} size="4x" />
+						<FontAwesomeIcon className="icon" icon={faHtml5} size="3x" />
 						<h3>HTML5</h3>
 					</div>
 					<div className="skills-box col-lg-3 col-sm-6">
-						<FontAwesomeIcon className="icon" icon={faCss3Alt} size="4x" />
+						<FontAwesomeIcon className="icon" icon={faCss3Alt} size="3x" />
 						<h3>CSS3</h3>
 					</div>
 					<div className="skills-box col-lg-3 col-sm-6">
-						<FontAwesomeIcon className="icon" icon={faBootstrap} size="4x" />
+						<FontAwesomeIcon className="icon" icon={faBootstrap} size="3x" />
 						<h3>Bootstrap</h3>
 					</div>
 					<div className="skills-box col-lg-3 col-sm-6">
-						<FontAwesomeIcon className="icon" icon={faJs} size="4x" />
+						<FontAwesomeIcon className="icon" icon={faJs} size="3x" />
 						<h3>JavaScript</h3>
 					</div>
 					<div className="skills-box col-lg-3 col-sm-6">
-						<FontAwesomeIcon className="icon" icon={faReact} size="4x" />
+						<FontAwesomeIcon className="icon" icon={faReact} size="3x" />
 						<h3>React</h3>
 					</div>
 					<div className="skills-box col-lg-3 col-sm-6">
-						<FontAwesomeIcon className="icon" icon={faJava} size="4x" />
+						<FontAwesomeIcon className="icon" icon={faJava} size="3x" />
 						<h3>Java</h3>
 					</div>
 					<div className="skills-box col-lg-3 col-sm-6">
-						<FontAwesomeIcon className="icon" icon={faTerminal} size="4x" />
+						<FontAwesomeIcon className="icon" icon={faTerminal} size="3x" />
 						<h3>Command Line</h3>
 					</div>
 					<div className="skills-box col-lg-3 col-sm-6">
-						<FontAwesomeIcon className="icon" icon={faGithub} size="4x" />
+						<FontAwesomeIcon className="icon" icon={faGithub} size="3x" />
 						<h3>GitHub</h3>
 					</div>
 					<div className="skills-box col-lg-3 col-sm-6">
@@ -54,18 +54,6 @@ const Skills = () => {
 					<div className="skills-box col-lg-3 col-sm-6">
 						<img className="icon-testing" src="../img/testng.png" alt="" /> <h3>TestNG</h3>
 					</div>
-					{/* <div className="skills-box col-lg-3 col-sm-6">
-						<FontAwesomeIcon icon={faGithub} size="4x" />
-						<h3>Jenkins</h3>
-					</div>
-					<div className="skills-box col-lg-3 col-sm-6">
-						<FontAwesomeIcon icon={faGithub} size="4x" />
-						<h3>Splunk</h3>
-					</div>
-					<div className="skills-box col-lg-3 col-sm-6">
-						<FontAwesomeIcon icon={faGithub} size="4x" />
-						<h3>Perfecto</h3>
-					</div> */}
 				</div>
 			</div>
 		</section>
