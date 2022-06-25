b2f6ea689223886a26a5b3e427585fbe30e0e04f
busrauygur <bnuygur@gmail.com>
2022-06-25 12:56:38 -0700

    Updates

---
+++ precache-manifest.42b9adf1247ce830efef47398a910544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7b68b70e090e8ec9c70",
+    "url": "/portfolio-project/static/css/main.8cb8152d.chunk.css"
+  },
+  {
+    "revision": "e7b68b70e090e8ec9c70",
+    "url": "/portfolio-project/static/js/main.e7b68b70.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/portfolio-project/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "1f8b57061568baf1e4f8",
+    "url": "/portfolio-project/static/js/2.1f8b5706.chunk.js"
+  },
+  {
+    "revision": "f1f416519303425b0e0d",
+    "url": "/portfolio-project/static/js/runtime~main.f1f41651.js"
+  },
+  {
+    "revision": "228c0be669437b4b38093bf6022cad06",
+    "url": "/portfolio-project/index.html"
+  }
+];
